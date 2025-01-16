cf3c773e2ffa5938ce0be1653a78908c76e729f4
devnydv <bittumailb2@gmail.com>
2025-01-17 00:24:43 +0530

    test

--- lowoncost/db.py
+++ lowoncost/db.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 import os
 
 envpath = find_dotenv()
+
 load_dotenv(envpath)
 url = os.getenv('db')
 client = MongoClient(url, socketTimeoutMS= 60000, connectTimeoutMS=60000)
