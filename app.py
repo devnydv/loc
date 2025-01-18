1f2b4d17971fc8fe7983a9043237a465e2096e24
devnydv <bittumailb2@gmail.com>
2025-01-18 18:31:04 +0530

    check

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from lowoncost.authfile import authfile
 from lowoncost.editdeal import editdeal
 from lowoncost.user import user
 
+
 app.register_blueprint(authfile)
 app.register_blueprint(editdeal)
 app.register_blueprint(user)
