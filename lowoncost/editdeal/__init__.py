4ed217c493983adda4be1d0f7a2ac51b0cfd02ec
devnydv <bittumailb2@gmail.com>
2025-01-18 18:23:35 +0530

    Enhance edit deal functionality, update category options, and add footer template

--- lowoncost/editdeal/__init__.py
+++ lowoncost/editdeal/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-editdeal = Blueprint("editdeal", __name__, template_folder='templates')
+editdeal = Blueprint("editdeal", __name__, template_folder='templates', static_folder='static', static_url_path='/editdeal/static')
 
 from lowoncost.editdeal import editdeals
