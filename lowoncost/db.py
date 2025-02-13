c43b6c868894665e641a7ab6fa0fa1005e66c9e0
devnydv <bittumailb2@gmail.com>
2025-02-13 23:55:57 +0530

    Sort deals by ID in descending order for improved retrieval; remove unnecessary reverse operation

--- lowoncost/db.py
+++ lowoncost/db.py
@@ -18,10 +18,10 @@ def get_deals(cat, page=1, page_size=12):
     collection = db.deals
     if cat == "all":
         #deals = list(collection.find({"status": "varified"}).skip(skip).limit(page_size))
-        deals = list(collection.find().skip(skip).limit(page_size))
+        deals = list(collection.find().sort('_id', -1).skip(skip).limit(page_size))
     else: 
         #deals = list(collection.find({"status": "varified", "category": cat}).skip(skip).limit(page_size))
         deals = list(collection.find({"category": cat}).skip(skip).limit(page_size))
     deals = json.loads(json_util.dumps(deals))
-    deals.reverse()
+    
     return deals
